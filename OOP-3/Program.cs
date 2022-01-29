54799e7630c9365b205d393883dd9835eace4e0e
ShevninEA <ev.shevnin@yandex.ru>
2022-01-29 18:39:14 +0300

    Задание № 3.1 выполнено

--- OOP-3/Program.cs
+++ OOP-3/Program.cs
@@ -6,7 +6,22 @@ namespace OOP_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("123");
+            BankAccount account1 = new BankAccount()
+            {
+                balance = 123,
+                number = 777
+            };
+            BankAccount account2 = new BankAccount()
+            {
+                balance = 580,
+                number = 111
+
+            };
+
+            account1.TransactionCoins(account2, 120);
+
+            account1.Info();
+            account2.Info();
         }
     }
 }
